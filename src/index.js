1e00a9af184631c77bb491ae4be53b9f08231052
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-29 12:57:51 -0300

    Param Types

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -10,6 +16,11 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +29,9 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id }  = request.params;
+
+  console.log(id);
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -27,6 +41,10 @@ app.put('/projects/:id', (request, response) => {
 });
 
 app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json([
     'Projeto 2',
     'Projeto 3',
